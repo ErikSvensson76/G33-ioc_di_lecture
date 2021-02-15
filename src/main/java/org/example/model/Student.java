5437089e2603c28a2c289437f74679b0ad4a7581
Erik Svensson <eosvensson@gmail.com>
2021-02-15 11:37:11 +0100

    fixed birthdate in Student.java

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,14 +1,15 @@
 package org.example.model;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Student {
     private String studentId;
     private String name;
-    private String birthDate;
+    private LocalDate birthDate;
     private String email;
 
-    public Student(String studentId, String name, String birthDate, String email) {
+    public Student(String studentId, String name, LocalDate birthDate, String email) {
         this.studentId = studentId;
         this.name = name;
         this.birthDate = birthDate;
@@ -30,11 +31,11 @@ public class Student {
         this.name = name;
     }
 
-    public String getBirthDate() {
+    public LocalDate getBirthDate() {
         return birthDate;
     }
 
-    public void setBirthDate(String birthDate) {
+    public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
 
