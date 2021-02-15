b01eb80b8fd32976fd14e3c2a965287cc56c95fa
Erik Svensson <eosvensson@gmail.com>
2021-02-15 10:32:27 +0100

    Configured IoC with xml

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,13 @@
 package org.example;
 
+import org.example.data.CourseDAO;
+import org.example.data.StudentDAO;
+import org.example.model.Course;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CourseDAO courseDAO = context.getBean(CourseDAO.class);
+        StudentDAO studentDAO = context.getBean(StudentDAO.class);
+
+        String id = UUID.randomUUID().toString();
+
+        courseDAO.persist(
+                new Course(id, "Java", LocalDate.parse("2021-02-15"), LocalDate.now().plusMonths(4), null)
+        );
+
+        System.out.println(courseDAO.findById(id));
+
+        context.close();
     }
 }
