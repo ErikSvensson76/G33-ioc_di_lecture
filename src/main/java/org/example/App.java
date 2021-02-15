59ab4b229e0417e3b164edbc14723fec82ecd131
Erik Svensson <eosvensson@gmail.com>
2021-02-15 10:47:14 +0100

    Configured IoC with annotations

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,13 @@
 package org.example;
 
+import org.example.config.ApplicationConfiguration;
+import org.example.data.StudentDAO;
+import org.example.model.Student;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+
+        String id = UUID.randomUUID().toString();
+
+        StudentDAO studentDAO = context.getBean(StudentDAO.class);
+        studentDAO.persist(new Student(
+                id, "Erik", LocalDate.now(), "erik@gmail.com"
+        ));
+
+        System.out.println(studentDAO.findById(id));
+
+        context.close();
     }
 }
