f45bdb42618e9c8207eee39e251e6e9a1a3d71dd
Erik Svensson <eosvensson@gmail.com>
2021-02-15 10:47:14 +0100

    Configured IoC with annotations

--- src/main/java/org/example/data/CourseDAOListImpl.java
+++ src/main/java/org/example/data/CourseDAOListImpl.java
@@ -1,11 +1,13 @@
 package org.example.data;
 
 import org.example.model.Course;
+import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+@Component
 public class CourseDAOListImpl implements CourseDAO{
 
     private Set<Course> courses;
